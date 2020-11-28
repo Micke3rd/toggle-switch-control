1ea9b3e32a9f7d975d44ae3da030304f7cc33079
Micke <Micke3rd@users.noreply.github.com>
2020-11-28 19:44:57 +0100

    format

--- Common/Utils/ScalarValueConverter.cs
+++ Common/Utils/ScalarValueConverter.cs
@@ -4,26 +4,26 @@ using System.Windows.Data;
 
 namespace Demo.Utils
 {
-	public class ScalarValueConverter : IValueConverter
-	{
-		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-		{
-			var oldValue = double.Parse(value.ToString(), culture);
-			if (parameter != null)
-			{
-				oldValue *= double.Parse(parameter.ToString(), culture);
-			}
-			return oldValue;
-		}
+    public class ScalarValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var oldValue = double.Parse(value.ToString(), culture);
+            if (parameter!=null)
+            {
+                oldValue*=double.Parse(parameter.ToString(), culture);
+            }
+            return oldValue;
+        }
 
-		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-		{
-			var oldValue = double.Parse(value.ToString(), culture);
-			if (parameter != null)
-			{
-				oldValue /= double.Parse(parameter.ToString(), culture);
-			}
-			return oldValue;
-		}
-	}
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var oldValue = double.Parse(value.ToString(), culture);
+            if (parameter!=null)
+            {
+                oldValue/=double.Parse(parameter.ToString(), culture);
+            }
+            return oldValue;
+        }
+    }
 }
